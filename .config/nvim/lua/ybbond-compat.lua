c05be6c40221164077c5cfc3304ae877b5418f02
Yohanes Bandung Bondowoso <hi@ybbond.id>
2022-03-05 16:47:09 +0700

    leverage neovim's aucmd and augroup capability

--- .config/nvim/lua/ybbond-compat.lua
+++ .config/nvim/lua/ybbond-compat.lua
@@ -2,37 +2,46 @@
 --                    COMPATIBILITIES
 ------------------------------------------------------------
 
-local au = require'au'
+vim.api.nvim_create_autocmd(
+  'TextYankPost',
+  {
+    callback = function()
+      vim.highlight.on_yank({ timeout = 400 })
+    end,
+  }
+)
 
-au({'TextYankPost'}, {
-  '*',
-  function()
-    vim.highlight.on_yank({ timeout = 400 })
-  end
-})
+vim.api.nvim_create_autocmd(
+  {'BufNewFile', 'BufRead'},
+  {
+    pattern = '*.c,*.cpp,*.cc,*.h,*.py',
+    callback = function()
+      vim.bo.tabstop = 4
+      vim.bo.shiftwidth = 4
+      vim.bo.expandtab = false
+    end,
+  }
+)
 
-au({'BufNewFile', 'BufRead'}, {
-  '*.c,*.cpp,*.cc,*.h,*.py',
-  function()
-    vim.bo.tabstop = 4
-    vim.bo.shiftwidth = 4
-    vim.bo.expandtab = false
-  end,
-})
+vim.api.nvim_create_autocmd(
+  {'BufNewFile', 'BufRead'},
+  {
+    pattern = '*.md,*.mmd,*.txt,*.markdown,*.multimarkdown',
+    callback = function()
+      vim.wo.wrap = true
+    end
+  }
+)
 
-au({'BufNewFile', 'BufRead'}, {
-  '*.md,*.mmd,*.txt,*.markdown,*.multimarkdown',
-  function()
-    vim.wo.wrap = true
-  end
-})
-
-au({'FileType'}, {
-  'qf',
-  function()
-    vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<CMD>cclose<CR>', {noremap = true})
-  end,
-})
+vim.api.nvim_create_autocmd(
+  'FileType',
+  {
+    pattern = 'qf',
+    callback = function()
+      vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<CMD>cclose<CR>', {noremap = true})
+    end,
+  }
+)
 
 vim.api.nvim_exec(
 [[
