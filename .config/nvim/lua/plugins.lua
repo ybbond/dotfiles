d3657c3fc5355aa5ae9ee629b448c1f7da1aadfb
Yohanes Bandung Bondowoso <hi@ybbond.id>
2023-07-13 12:47:44 +0700

    use single quote

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -63,12 +63,12 @@ return {
   },
 
   {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
+    'utilyre/barbecue.nvim',
+    name = 'barbecue',
+    version = '*',
     dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons",
+      'SmiteshP/nvim-navic',
+      'nvim-tree/nvim-web-devicons',
     },
     opts = {
       -- configurations go here
